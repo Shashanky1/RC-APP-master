d7c043c416ef5a554bc3e2ada624d6cdffef8991
Shashanky1 <shashanky665@gmail.com>
2025-07-23 13:21:08 +0530

    Update SocketContext.jsx

--- RC-APP-master/frontend/src/context/SocketContext.jsx
+++ RC-APP-master/frontend/src/context/SocketContext.jsx
@@ -14,7 +14,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("https://rc-app2.onrender.com/", {
+      const socket = io("https://rc-app-master.onrender.com/", {
         query: {
           userId: authUser._id,
         }
